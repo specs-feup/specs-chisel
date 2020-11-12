3f78b8a05b1574eab01aa82dde8fdc186cba26e6
nmcp88 <nmcp88@gmail.com>
2020-11-12 16:49:15 +0000

    [specs-chisel] top level workspace restructure (still testing config)

--- FunctionalUnits/src/generators/ModuleGenerator.scala
+++ FunctionalUnits/src/generators/ModuleGenerator.scala
@@ -3,12 +3,16 @@ package generators
 import java.io.File
 
 import chisel3.stage.ChiselStage
+import modules.{FunctionalUnit, LeftShift}
 
 object ModuleGenerator extends App {
-    def generate(unit: FunctionalUnit, targetDir : String) {
+    def generate(unit: FunctionalUnit, targetDir: String) {
         val path1 = new File(targetDir)
         val cs = new ChiselStage
         cs.emitVerilog(unit,
             Array("--target-dir", path1.getAbsolutePath, "-o", unit.getName()))
     }
+
+    // main (?)
+    ModuleGenerator.generate(new LeftShift(32), "./output/");
 }
