2cabd70595acb0bd1be8b58de9bd4574558231f5
nmcp88 <nmcp88@gmail.com>
2020-11-24 14:51:21 +0000

    [utils] wip on generic mux class

--- utils/main/pt/inesctec/genericmux/GenericMux.scala
+++ utils/main/pt/inesctec/genericmux/GenericMux.scala
@@ -1,12 +1,12 @@
 package pt.inesctec.genericmux
 
-import Chisel.{Mux1H, MuxCase}
+import Chisel.{Cat, Mux1H, MuxCase}
 import chisel3.core.fromIntToWidth
-import chisel3.{Bundle, Flipped, Input, Output, RawModule, UInt, Vec, fromIntToLiteral}
+import chisel3.{Bundle, Flipped, Input, Module, Output, RawModule, UInt, Vec, fromIntToLiteral}
 
 class Test extends Bundle {
-  val valuein = Input(UInt(32.W))
-  //val valueout = Output(UInt(32.W))
+  val valuein = Input(UInt(8.W))
+  val valueout = Output(UInt(8.W))
 }
 
 class MuxIO(nins: Int) extends Bundle {
@@ -15,6 +15,41 @@ class MuxIO(nins: Int) extends Bundle {
   val output = Flipped(new Test)
 
   override def cloneType: MuxIO.this.type = new MuxIO(nins).asInstanceOf[this.type]
+}
+
+class CrossbarIO(nins: Int) extends Bundle {
+  val select = Input(UInt((nins * nins).W))
+  val inputs = Vec(nins, new Test)
+  val output = Flipped(Vec(nins, new Test))
+
+  override def cloneType: CrossbarIO.this.type = new CrossbarIO(nins).asInstanceOf[this.type]
+}
+
+object GenericCrossBar {
+  // default mux of 2 UInts
+  def apply(nins: Int) = {
+    new GenericCrossBar(new CrossbarIO(nins))
+  }
+}
+
+protected class GenericCrossBar(crossio: CrossbarIO) extends RawModule {
+  val io = IO(crossio)
+  for (i <- io.output.indices) {
+    val mux = Module(GenericMux(io.inputs.size))
+    mux.io.inputs <> io.inputs
+    mux.io.select := io.select(io.inputs.size * (i + 1) - 1, io.inputs.size * i)
+    io.output(i) <> mux.io.output
+  }
+
+  for (i <- io.inputs.indices) {
+    val mux = Module(GenericMux(io.output.size))
+    mux.io.inputs <> Flipped(io.output)
+    var bitsel = io.select(i).asUInt()
+    for(i <- io.output.indices)
+      bitsel = Cat(bitsel, io.select((i + 1) * io.inputs.size))
+    mux.io.select := bitsel
+    io.inputs(i) <> mux.io.output
+  }
 }
 
 object GenericMux {
@@ -26,12 +61,60 @@ object GenericMux {
 
 protected class GenericMux(muxio: MuxIO) extends RawModule {
   val io = IO(muxio)
-  //val arraySel = (io.inputs.indices map { i => io.select(i) -> io.inputs(i) })
-  //io.output := MuxCase(0.U.asTypeOf(io.output), arraySel)
 
-  //(io.select.asBools() zip io.inputs).map{ case (sel, in) => if(sel == true.B) io.output <> in }
-
+  //This works!
+  for (i <- io.inputs.indices) {
+    io.inputs(i) <> Mux1H(Seq((io.select.asBools()(i), io.output)))
+  }
   io.output <> Mux1H(io.select.asBools() zip io.inputs)
+
+  /*
+    var combine = 0.U.asTypeOf(io.inputs(0))
+    for (i <- io.inputs.indices) {
+      combine = Mux(io.select(i).asBool(), io.inputs(i), combine)
+      // this doesnt work because type T must contain unidirectional stuff
+    }
+    io.output <> combine
+  */
+
+  /*
+  //val choice: List[Test] = List.fill(io.inputs.size)(Wire(new Test))
+
+  when(io.select.asUInt().orR === false.B) {
+    io.output <> 0.U.asTypeOf(io.output)
+  }
+
+  for (i <- io.inputs.indices) {
+    when(io.select(i) === true.B) {
+      //choice(i) := io.inputs(i)
+      io.inputs(i) <> io.output
+    }.otherwise {
+      //choice(i) := 0.U.asTypeOf(io.inputs(i))
+      io.inputs(i) <> 0.U.asTypeOf(io.inputs(i))
+    }
+  }*/
+
+  //io.output <>
+
+  /*for (i <- io.inputs.indices) {
+    when(io.select(i) === true.B) {
+      io.output <> io.inputs(i)
+
+    }.elsewhen(io.select.asUInt().orR === false.B) {
+      io.output <> 0.U.asTypeOf(io.output)
+      io.inputs(i) <> 0.U.asTypeOf(io.inputs(i))
+    }
+  }*/
+
+
+  /*
+  io.output <> Mux1H(io.select.asBools() zip io.inputs)
+  for (i <- io.inputs.indices) {
+    when(io.select(i) === false.B) {
+      io.inputs(i) <> 0.U.asTypeOf(io.inputs(i))
+    }
+  }
+   */
 }
 
 class UIntMux(nins: Int) extends RawModule {
--- utils/test/pt/inesctec/ModuleGenerator.scala
+++ utils/test/pt/inesctec/ModuleGenerator.scala
@@ -3,14 +3,14 @@ package pt.inesctec
 import java.io.File
 
 import chisel3.stage.ChiselStage
-import pt.inesctec.genericmux.GenericMux
+import pt.inesctec.genericmux.GenericCrossBar
 
 object ModuleGenerator extends App {
 
   val path1 = new File("./utils/output/")
   val cs = new ChiselStage
-  cs.emitVerilog(GenericMux(2),
-    Array("--target-dir", path1.getAbsolutePath, "-o", "GenericMux"))
+  cs.emitVerilog(GenericCrossBar(2),
+    Array("--target-dir", path1.getAbsolutePath, "-o", "GenericCrossBar"))
 
   //(new ChiselStage).execute(Array[String](), Seq(ChiselGeneratorAnnotation(() => new TestDataMirror)))
 }
