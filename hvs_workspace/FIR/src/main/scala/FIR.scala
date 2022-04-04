06d8172d50aa678660ab1b8f6672ce33bc7a145d
HenriqueVsousa <henrique.veloso.de.sousa@gmail.com>
2022-04-04 15:34:18 +0100

    rocket commit

--- hvs_workspace/FIR/src/main/scala/FIR.scala
+++ hvs_workspace/FIR/src/main/scala/FIR.scala
@@ -13,7 +13,6 @@ class FIR(length : Int) extends Module{
      val recv_signal = Input(Bool())
      val exec_signal = Input(Bool())
      val out = Output(UInt(32.W))
-     val sum = Output(UInt(32.W))
    })
 
   val coeffs = RegInit(VecInit(Seq.fill(length)(0.U(32.W))))
@@ -33,7 +32,6 @@ class FIR(length : Int) extends Module{
     result := taps.zip(coeffs).map { case (a, b) => a * b }.reduce(_ + _)
   }
 
-  io.sum := Mux(coeff_count === length.U, coeffs.reduce(_+_), 0.U)
   io.out :=  result
 
 }
@@ -44,34 +42,52 @@ class test extends AnyFlatSpec with ChiselScalatestTester{
       f.io.data.poke(43.U)
       f.io.recv_signal.poke(true.B)
       f.clock.step(1)
-      f.io.recv_signal.poke(false.B)
+      //f.io.recv_signal.poke(false.B)
 
       f.io.data.poke(32.U)
-      f.io.recv_signal.poke(true.B)
+     // f.io.recv_signal.poke(true.B)
       f.clock.step(1)
-      f.io.recv_signal.poke(false.B)
+      //f.io.recv_signal.poke(false.B)
 
       f.io.data.poke(17.U)
-      f.io.recv_signal.poke(true.B)
+      //f.io.recv_signal.poke(true.B)
       f.clock.step(1)
-      f.io.recv_signal.poke(false.B)
+      //f.io.recv_signal.poke(false.B)
 
       f.io.data.poke(79.U)
-      f.io.recv_signal.poke(true.B)
+      //f.io.recv_signal.poke(true.B)
       f.clock.step(1)
-      f.io.recv_signal.poke(false.B)
-      f.io.sum.expect(171.U)
+      //f.io.recv_signal.poke(false.B)
 
       f.io.data.poke(12.U)
+     // f.io.recv_signal.poke(true.B)
+      f.clock.step(1)
+      f.io.recv_signal.poke(false.B)
+
+      f.io.exec_signal.poke(true.B)
+      f.clock.step(1)
+      f.io.exec_signal.poke(false.B)
+      f.io.out.expect(516.U)
+
+      f.io.data.poke(21.U)
       f.io.recv_signal.poke(true.B)
       f.clock.step(1)
       f.io.recv_signal.poke(false.B)
 
       f.io.exec_signal.poke(true.B)
-      f.clock.step(4)
+      f.clock.step(1)
       f.io.exec_signal.poke(false.B)
-    //  f.clock.step(3)
-      f.io.out.expect(516.U)
+      f.io.out.expect(1287.U)
+
+      f.io.data.poke(33.U)
+      f.io.recv_signal.poke(true.B)
+      f.clock.step(1)
+      f.io.recv_signal.poke(false.B)
+
+      f.io.exec_signal.poke(true.B)
+      f.clock.step(1)
+      f.io.exec_signal.poke(false.B)
+      f.io.out.expect(2295.U)
     }
   }
 }
