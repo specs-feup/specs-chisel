4df98b23bef3106f376ff8640f83ec120f08bad8
nmcp88 <nmcp88@gmail.com>
2023-02-03 23:49:03 +0000

    [specs-chisel] removed more build artifacts

--- hvs_workspace/rocket_generator/accelerators/fir/module/test_run_dir/scie.SCIETesterStage1863198209/firrtl_black_box_resource_files.f
+++
@@ -1,2 +0,0 @@
-/home/henrique/Desktop/fir/test_run_dir/scie.SCIETesterStage1863198209/BBFAdd.v
-/home/henrique/Desktop/fir/test_run_dir/scie.SCIETesterStage1863198209/BBFMultiply.v
--- hvs_workspace/rocket_generator/accelerators/fir/module/test_run_dir/scie.SCIETesterStage70611531/firrtl_black_box_resource_files.f
+++
@@ -1,2 +0,0 @@
-/home/henrique/Desktop/fir/test_run_dir/scie.SCIETesterStage70611531/BBFAdd.v
-/home/henrique/Desktop/fir/test_run_dir/scie.SCIETesterStage70611531/BBFMultiply.v
